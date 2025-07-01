cfdfdee7de2b9a5f1216a9812ef0d07591f71263
jadeevelyn06 <rey24011@byui.edu>
2025-06-30 22:28:26 -0600

    nav fixes

--- coolpics/coolpics.js
+++ coolpics/coolpics.js
@@ -1,10 +1,28 @@
+
+//this is dumb. this didnt work
+//but when i rewrote it the same exact way, it did work???????!!!?!?!?!?
+//javascript makes me sad. 
+//const menuButton = document.getElementById('menu-button');
+//const nav = document.getElementById('main-nav');
+
+//menuButton.addEventListener('click', () => {
+ // nav.classList.toggle('open');
+//});
+//const menuButton = document.getElementById('menu-button');
+//const nav = document.getElementById('main-nav');
+
+//menuButton.addEventListener('click', () => {
+  // Only toggle if screen is medium
+  //if (window.innerWidth >= 700 && window.innerWidth < 1000) {
+  //  nav.classList.toggle('open');
+  //}
+//});
+
 const menuButton = document.getElementById('menu-button');
-const navList = document.getElementById('main-nav').querySelector('ul');
+const nav = document.getElementById('main-nav');
 
 menuButton.addEventListener('click', () => {
-  if (navList.style.display === 'block') {
-    navList.style.display = 'none';
-  } else {
-    navList.style.display = 'block';
+  if (window.innerWidth >= 700 && window.innerWidth < 1000) {
+    nav.classList.toggle('open');
   }
 });
