395efa48ce31e764ded0ec38978dba929c4259e8
jadeevelyn06 <rey24011@byui.edu>
2025-06-30 22:39:49 -0600

    comments

--- coolpics/coolpics.js
+++ coolpics/coolpics.js
@@ -2,21 +2,12 @@
 //this is dumb. this didnt work
 //but when i rewrote it the same exact way, it did work???????!!!?!?!?!?
 //javascript makes me sad. 
-//const menuButton = document.getElementById('menu-button');
-//const nav = document.getElementById('main-nav');
 
-//menuButton.addEventListener('click', () => {
- // nav.classList.toggle('open');
-//});
-//const menuButton = document.getElementById('menu-button');
-//const nav = document.getElementById('main-nav');
-
-//menuButton.addEventListener('click', () => {
-  // Only toggle if screen is medium
-  //if (window.innerWidth >= 700 && window.innerWidth < 1000) {
-  //  nav.classList.toggle('open');
-  //}
-//});
+//the way none of this makes any sense at all. 
+//i watched like 10 youtube videos today and nothing helped. 
+//0 of the info went into my brain i think . 
+//ugh.
+//}); lol
 
 const menuButton = document.getElementById('menu-button');
 const nav = document.getElementById('main-nav');
